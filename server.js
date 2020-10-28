f040e6c794459a5d8e96fc169d146c6cce6bf081
Aubrey Polk <aubstacle3@gmail.com>
2020-10-27 23:53:27 -0400

    added functioning service worker and favicon

--- server.js
+++ server.js
@@ -17,13 +17,12 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}`);
+  console.log(`App running on port ${PORT}!`);
 });
